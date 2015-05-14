e5f5ec6d42d305f4b462649eb3f6391ea0eca3c1
Ira W. Snyder <isnyder@lcogt.net>
2015-05-14 10:31:42 -0700

    Add missing www.lcogt.net hostname
    The ALLOWED_HOSTS setting describes the hostnames that this site can be
    accessed as. We need both lcogt.net and www.lcogt.net.

--- lcogt_mezzanine/settings.py
+++ lcogt_mezzanine/settings.py
@@ -111,7 +111,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','lcogt.net','docknode06.lco.gtn','dockerhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','lcogt.net','www.lcogt.net','docknode06.lco.gtn','dockerhost']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
